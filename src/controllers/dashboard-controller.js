0bc23ab447921a704f3a54bf4634cdae7038627e
Frank Donnellan <97000920+frankied67@users.noreply.github.com>
2024-03-03 12:45:52 +0000

    accounts-controller added and dashboard controller updated

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,7 +1,24 @@
+import { db } from "../models/db.js";
+
 export const dashboardController = {
   index: {
     handler: async function (request, h) {
-      return h.view("main");
+      const locations = await db.locationStore.getAllLocations();
+      const viewData = {
+        title: "Playmark Dashboard",
+        locations: locations,
+      };
+      return h.view("dashboard-view", viewData);
+    },
+  },
+
+  addLocation: {
+    handler: async function (request, h) {
+      const newLocation = {
+        title: request.payload.title,
+      };
+      await db.locationStore.addLocation(newLocation);
+      return h.redirect("/dashboard");
     },
   },
 };
