923978173049bb8a6dfa6a922fcbe2327a046e09
Frank Donnellan <97000920+frankied67@users.noreply.github.com>
2024-03-05 23:14:52 +0000

    Json store added and engaged

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,10 @@
-import { userMemStore } from "./mem/user-mem-store.js";
-import { locationMemStore } from "./mem/location-mem-store.js";
-import { monumentMemStore } from "./mem/monument-mem-store.js";
+// import { userMemStore } from "./mem/user-mem-store.js";
+// import { locationMemStore } from "./mem/location-mem-store.js";
+// import { monumentMemStore } from "./mem/monument-mem-store.js";
+
+import { userJsonStore } from "./json/user-json-store.js";
+import { locationJsonStore } from "./json/location-json-store.js";
+import { monumentJsonStore } from "./json/monument-json-store.js";
 
 export const db = {
   userStore: null,
@@ -8,8 +12,8 @@ export const db = {
   monumentStore: null,
 
   init() {
-    this.userStore = userMemStore;
-    this.locationStore = locationMemStore;
-    this.monumentStore = monumentMemStore;
+    this.userStore = userJsonStore;
+    this.locationStore = locationJsonStore;
+    this.monumentStore = monumentJsonStore;
   },
 };
